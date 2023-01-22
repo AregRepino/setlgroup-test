2cb1db72c70ed84ccf55641c79d059ba5020fee5
areg <test@gmail.com>
2023-01-22 18:25:43 +0300

    fix

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Diamond diamond = new Diamond();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Видите высоту от 1 до 80");
+        System.out.println("Введите высоту от 1 до 80");
         diamond.setHeight(scanner.nextInt());
         System.out.println("Введите ширину от 1 до 80");
         diamond.setWidth(scanner.nextInt());
